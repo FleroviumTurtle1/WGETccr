b0cb7dd6c45d3ce60795250efc61409f908cdb2d
JaggedZirconium <67721548+JaggedZirconium@users.noreply.github.com>
2022-11-01 11:22:27 -0500

    delete and reset for installation

--- create.lua
+++ create.lua
@@ -1,2 +1,12 @@
-shell.run("wget https://raw.githubusercontent.com/JaggedZirconium/WGETccr/main/socket/main.lua socket/main.lua")
-shell.run("wget https://raw.githubusercontent.com/JaggedZirconium/WGETccr/main/startup/re.lua startup/re.lua")
+if not fs.exists('startup/re.lua') then
+    shell.run("wget https://raw.githubusercontent.com/JaggedZirconium/WGETccr/main/startup/re.lua startup/re.lua")
+else
+    fs.delete('startup/re.lua')
+    shell.run("wget https://raw.githubusercontent.com/JaggedZirconium/WGETccr/main/startup/re.lua startup/re.lua")
+end
+if not fs.exists('socket/main.lua') then
+    shell.run("wget https://raw.githubusercontent.com/JaggedZirconium/WGETccr/main/socket/main.lua socket/main.lua")
+else
+    fs.delete('socket/main.lua')
+    shell.run("wget https://raw.githubusercontent.com/JaggedZirconium/WGETccr/main/socket/main.lua socket/main.lua")
+end
