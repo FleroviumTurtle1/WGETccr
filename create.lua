51da9ef04c195c2517627a9a2846ee58fbcf0e94
JaggedZirconium <67721548+JaggedZirconium@users.noreply.github.com>
2022-11-01 13:56:13 -0500

    reboot not working?

--- create.lua
+++ create.lua
@@ -1,24 +1,35 @@
 os.pullEvent(); = os.pullEventRaw();
+local z = 0
 if fs.exists('startup') then
     fs.delete('startup')
 end
 if not fs.exists('startup.lua') then
+    z = z + 1
     shell.run("wget https://raw.githubusercontent.com/JaggedZirconium/WGETccr/main/startup.lua startup.lua")
 else
+    z = z + 1
     fs.delete('startup.lua')
     shell.run("wget https://raw.githubusercontent.com/JaggedZirconium/WGETccr/main/startup.lua startup.lua")
 end
 if not fs.exists('start/re.lua') then
+    z = z + 1
     shell.run("wget https://raw.githubusercontent.com/JaggedZirconium/WGETccr/main/start/re.lua start/re.lua")
 else
+    z = z + 1
     fs.delete('start/re.lua')
     shell.run("wget https://raw.githubusercontent.com/JaggedZirconium/WGETccr/main/start/re.lua start/re.lua")
 end
 if not fs.exists('socket/main.lua') then
+    z = z + 1
     shell.run("wget https://raw.githubusercontent.com/JaggedZirconium/WGETccr/main/socket/main.lua socket/main.lua")
 else
+    z = z + 1
     fs.delete('socket/main.lua')
     shell.run("wget https://raw.githubusercontent.com/JaggedZirconium/WGETccr/main/socket/main.lua socket/main.lua")
 end
-sleep(1)
-os.reboot()
+while z < 3 do
+    sleep(0.2)
+end
+if z == 3 then
+    os.reboot()
+end
