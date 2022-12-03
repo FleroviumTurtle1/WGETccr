32367cb95b509dca1d48895f3ed48e27c64307c4
JaggedZirconium <67721548+JaggedZirconium@users.noreply.github.com>
2022-12-03 15:32:24 -0600

    cords

--- startup/re.lua
+++ startup/re.lua
@@ -1,6 +1,15 @@
 local oldPull = os.pullEvent;
 os.pullEvent = os.pullEventRaw;
 local h = 0
+if not fs.exists("cords.txt") then
+    tem = fs.open("cords.txt","w")
+    write("Cords(1,2,3): ")
+    z1 = tostring(read())
+    write("Facing(1/n,2/e,3/s,4/s): ")
+    z2 = tostring(read())
+    tem.write(z1.."\n"..z2)
+    tem.close()
+end
 if fs.exists("socket/main.lua") then
     tester = fs.open("socket/main.lua","r")
     tex = tester.readAll()
