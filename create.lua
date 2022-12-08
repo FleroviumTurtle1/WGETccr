5bb16a2dbee5737ad1939249d5c8c15de15b2a1a
JaggedZirconium <67721548+JaggedZirconium@users.noreply.github.com>
2022-12-08 09:16:18 -0600

    removal of re = 0

--- create.lua
+++ create.lua
@@ -1,30 +1,17 @@
 os.pullEvent = os.pullEventRaw;
-re = 0
 if fs.exists('startup.lua') then
     fs.delete('startup.lua')
 end
-function s2()
-    if not fs.exists('startup/re.lua') then
-        shell.run("wget https://raw.githubusercontent.com/JaggedZirconium/WGETccr/main/startup/re.lua startup/re.lua")
-    else
-        fs.delete('startup/re.lua')
-        shell.run("wget https://raw.githubusercontent.com/JaggedZirconium/WGETccr/main/startup/re.lua startup/re.lua")
-    end
-    re = re + 1
+if not fs.exists('startup/re.lua') then
+    shell.run("wget https://raw.githubusercontent.com/JaggedZirconium/WGETccr/main/startup/re.lua startup/re.lua")
+else
+    fs.delete('startup/re.lua')
+    shell.run("wget https://raw.githubusercontent.com/JaggedZirconium/WGETccr/main/startup/re.lua startup/re.lua")
 end
-function s3()
-    if not fs.exists('socket/main.lua') then
-        shell.run("wget https://raw.githubusercontent.com/JaggedZirconium/WGETccr/main/socket/main.lua socket/main.lua")
-    else
-        fs.delete('socket/main.lua')
-        shell.run("wget https://raw.githubusercontent.com/JaggedZirconium/WGETccr/main/socket/main.lua socket/main.lua")
-    end
-    re = re + 1
+if not fs.exists('socket/main.lua') then
+    shell.run("wget https://raw.githubusercontent.com/JaggedZirconium/WGETccr/main/socket/main.lua socket/main.lua")
+else
+    fs.delete('socket/main.lua')
+    shell.run("wget https://raw.githubusercontent.com/JaggedZirconium/WGETccr/main/socket/main.lua socket/main.lua")
 end
-function s4()
-    while not re == 2 do
-        coroutine.yield()
-    end
-    os.reboot()
-end
-parallel.waitForAll(s2,s3)
+os.reboot()
